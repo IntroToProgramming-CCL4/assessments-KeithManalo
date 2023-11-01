15844af7eafa87a6e539d7bc5ac5669acbfc633b
KeithManalo <145595259+KeithManalo@users.noreply.github.com>
2023-11-01 19:41:02 +0400

    Update Chap.1-excer.1.py

--- Assessment 1/Chapter 1- Getting Started with Python/Exercises/Chap.1-excer.1.py
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Chap.1-excer.1.py
@@ -1,1 +1,2 @@
+#using print('') will print any thing inside the qutiation mark ''
 print('Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are')
