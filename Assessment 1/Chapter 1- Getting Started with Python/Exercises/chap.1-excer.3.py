0b6e0f21ea2db1618421ef7cfde82ad4343d6f5d
KeithManalo <145595259+KeithManalo@users.noreply.github.com>
2023-11-01 20:24:55 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/chap.1-excer.3.py
@@ -0,0 +1,7 @@
+# import method
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+ 
+print("now =", now)
---
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/chap.1-excer.4.py
@@ -0,0 +1,11 @@
+'''v1 is the variable
+the equal sign is the assigning operator
+after that would be the content you want inside the variable
+plus sign is to connect or you could also use comma instead'''
+
+v1='hello!, '
+v2='good day to you, '
+v3='how are you?.'
+
+print(v1+v2+v3)
+
